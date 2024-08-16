7383cfb8cff7d73225dc461746d1a9e76daa37a1
joshua <omodiagbejoshuapti@gmail.com>
2024-08-16 13:34:04 +0100

    tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,6 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-<<<<<<< HEAD
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -11,7 +10,7 @@ const config: Config = {
   theme: {
     extend: {
       screens: {
-        xs: { max: '420px' }, // Custom breakpoint for mobile screens 400px and below
+        xs: { max: '400px' }, // Custom breakpoint for mobile screens 400px and below
         sm: '640px', // Default Tailwind small screen breakpoint
         md: '768px', // Default Tailwind medium screen breakpoint
         lg: '1024px', // Default Tailwind large screen breakpoint
@@ -28,47 +27,14 @@ const config: Config = {
         baseBlue: '#3064B6',
         subBlue: '#1358CD',
         input: '#1358CD26',
-        customBlue: '#1358CD',
+      },
+      fontFamily: {
+        suisseWorks: ['SuisseWorks', 'sans-serif'],
+        suisseIntl: ['SuisseIntl', 'serif'],
       },
     },
   },
   plugins: [],
-=======
-	content: [
-		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
-		"./components/**/*.{js,ts,jsx,tsx,mdx}",
-		"./app/**/*.{js,ts,jsx,tsx,mdx}",
-		"./ui/**/*.{js,ts,jsx,tsx,mdx}",
-	],
-	theme: {
-		extend: {
-			screens: {
-				xs: { max: "400px" }, // Custom breakpoint for mobile screens 400px and below
-				sm: "640px", // Default Tailwind small screen breakpoint
-				md: "768px", // Default Tailwind medium screen breakpoint
-				lg: "1024px", // Default Tailwind large screen breakpoint
-				xl: "1280px", // Default Tailwind extra large screen breakpoint
-				"2xl": "1536px", // Default Tailwind 2x extra large screen breakpoint
-			},
-			backgroundImage: {
-				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-				"gradient-conic":
-					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-			},
-			colors: {
-				baseGray: "#161C2D",
-				baseBlue: "#3064B6",
-				subBlue: "#1358CD",
-				input: "#1358CD26",
-			},
-			fontFamily: {
-				suisseWorks: ["SuisseWorks", "sans-serif"],
-				suisseIntl: ["SuisseIntl", "serif"],
-			},
-		},
-	},
-	plugins: [],
->>>>>>> 6178da46671bf95aaf2ae259cf816ee01894f151
 };
 
 export default config;

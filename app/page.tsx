23e9f44fb959e43c3579c4f99bb15987e28dc08b
joshua <omodiagbejoshuapti@gmail.com>
2024-08-17 14:27:40 +0100

    heart commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import '@/app/globals.css';
 
 import blueCard from '@/public/bluecard.png';
+
 import Button from '@/ui/Button';
 import desktopCard from '@/public/Subtract (2).png';
 import halfImage from '@/public/half-image.png';
@@ -17,6 +18,8 @@ import subBlue from '@/public/bluesub.png';
 import subBlack from '@/public/Subtract dark (1).png';
 import circle from '@/public/circleblack.png';
 import arrow from '@/public/arrow.png';
+import whiteBg from '@/public/subWhite.png';
+import bigBlue from '@/public/bigBlue.png';
 
 export default function Home() {
   return (
@@ -143,13 +146,14 @@ export default function Home() {
           className='hidden xs:block my-10 rounded-3xl'
         />
       </div>
-      <div className=' my-16 flex w-[90%] mx-auto justify-evenly gap-9'>
-        <div className='w-[587px] h-[587px] bg-black relative rounded-[40px] shadow-xl'>
-          <h1 className='text-white absolute left-10 top-20 font-bold text-3xl'>
+      {/* first set */}
+      <div className=' my-16 flex xs:flex-col w-[90%] mx-auto justify-between xs:justify-center align-middle gap-9'>
+        <div className='w-[587px] h-[594px] xs:w-[371px] xs:h-[375.42px] bg-black relative rounded-[40px] shadow-xl'>
+          <h1 className='text-white absolute left-10 top-14 font-bold text-3xl xs:text-lg'>
             Innovative <br />
             Solutions
           </h1>
-          <p className='text-white absolute leading-5 left-10 top-40 font-light w-[40%]'>
+          <p className='text-white absolute leading-5 left-10 top-28 font-light w-[50%] xs:text-[11px] xs:w-[45%]'>
             Ledga offers unique futures like internal wallets, peer transfers,
             sub-accounts, and automated savings, ensuring all your financial
             needs are met.
@@ -163,7 +167,7 @@ export default function Home() {
             className='w-[46%] absolute bottom-0 right-0 rounded-br-3xl'
           />
         </div>
-        <div className='relative w-[587px] h-[587px] shadow-xl rounded-[40px] '>
+        <div className='relative w-[587px] h-[587px] xs:w-[370px] xs:h-[375px] shadow-xl rounded-[40px] '>
           {/* Hand image at the background */}
           <Image
             src={hand}
@@ -180,10 +184,10 @@ export default function Home() {
             objectFit='cover'
             className='absolute inset-0 z-10 rounded-[40px] shadow-md'
           />
-          <h1 className='absolute top-96 left-[67px] text-white font-bold text-2xl z-10 leading-6'>
+          <h1 className='absolute top-96 left-[67px] text-white font-bold xs:text-lg text-2xl z-10 leading-6 xs:top-56 xs:left-9'>
             Experience Seamless <br /> Payments with Ease
           </h1>
-          <p className='absolute top-[75%] left-[67px] text-white font-light z-10 text- leading-5 w-[50%]'>
+          <p className='absolute top-[75%] xs:text-[11px] left-[67px] text-white font-light z-10 xs:w-[80%] xs:left-9 leading-5 w-[50%]'>
             Make payments, settle bills, and send money to friends effortlessly.
             Fast, free, and secure—discover the future of hassle-free
             transactions today. Join us and simplify your financial life now!
@@ -193,49 +197,199 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className='flex w-[90%] mx-auto gap-9'>
-        <div className='relative'>
+      {/* next set */}
+      <div className='flex w-[90%] xs:flex-col mx-auto gap-9 xs:grid'>
+        <div
+          className='relative bg-[#F3F7FC] rounded-[50px] 
+         '
+        >
           <Image
             src={subBlue}
             alt='blue background'
-            className='w-[577.87px] h-[584.76px]'
+            className='w-[587px] h-[594px]  relative xs:w-[370px] xs:h-[374.41px]'
           />
-          <p className='absolute top-12  left-28 font-semibold text-[20px] text-white '>
+          <p className='absolute top-12  left-28 xs:left-16 font-semibold text-[20px] text-white '>
             Market size
           </p>
-          <div className='absolute top-20 left-28 text-white '>
-            <h1 className='font-bold text-[70px] '>264</h1>
-            <p className='text-[15px] mt-[-20px] font-light'>
-              Tertiary Institution{' '}
+          <div className='absolute xs:left-16 top-20 left-28 xs:top-20 text-white '>
+            <h1 className='font-bold text-[70px]  xs:text-[40px] '>264</h1>
+            <p className='text-[15px] mt-[-20px] font-light xs:mt-[-10px]'>
+              Tertiary Institution
             </p>
           </div>
-          <div className='absolute top-48 left-28 text-white '>
-            <h1 className='font-bold text-[70px] '>104.6K</h1>
-            <p className='text-[15px] mt-[-20px] font-light '>Companies</p>
+          <div className='absolute xs:left-16 top-48 xs:top-40 left-28 text-white '>
+            <h1 className='font-bold text-[70px] xs:text-[40px] '>104.6K</h1>
+            <p className='text-[15px] mt-[-20px] font-light xs:mt-[-10px] '>
+              Companies
+            </p>
           </div>
-          <div className='absolute top-80 left-28 text-white '>
-            <h1 className='font-bold text-[70px] '>1.8m</h1>
-            <p className={`text-[15px] mt-[-20px] font-light `}>Students</p>
+          <div className='absolute xs:left-16 top-80 left-28 text-white xs:top-60'>
+            <h1 className='font-bold text-[70px] xs:text-[40px]  '>1.8m</h1>
+            <p className={`text-[15px] mt-[-20px] font-light xs:mt-[-10px] `}>
+              Students
+            </p>
           </div>
           <div>
             <Image
               src={circle}
               alt='circle'
-              className='absolute top-4 right-12 w-[108px] h-[106px]'
+              className='absolute top-4 right-12 w-[108px] h-[106px] xs:w-[60px] xs:h-[59px]'
             />
             <Image
               src={arrow}
               alt='arrow'
-              className='absolute top-[3.25rem] right-[83px] w-[37px] h-[36px]'
+              className='absolute top-[3.25rem] right-[83px] xs:top-[2.25rem] xs:right-[69px] w-[37px] h-[36px] xs:w-[20px] xs:h-[20px]'
             />
           </div>
         </div>
-        <div>
+        <div className='relative'>
           <Image
             src={subBlack}
             alt='black background'
-            className='w-[566.87px] h-[368px]'
+            className='w-[566.87px] h-[368px] xs:w-[377.45px] xs:h-[245px]'
           />
+          <div className='absolute top-20 left-16 text-white xs:top-9'>
+            <h1 className=' font-bold text-[70px] xs:text-[40px]'>N4.8m</h1>
+            <p className='text-[15px] mt-[-20px] xs:mt-[-10px] w-[50%] xs:text-[15px] font-light '>
+              Since January 2024, Ledga has processed over 4 million naira worth
+              of transactions
+            </p>
+          </div>
+          <div className='bg-[#F3F7FC] w-[575px] h-[179px] rounded-[50px] xs:w-[377px] xs:h-[117px] gap-8 xs:gap-6 mt-[47px] flex justify-center items-center'>
+            <h1 className=' font-bold text-subBlue text-[70px] xs:text-[40px]'>
+              70K
+            </h1>
+            <p className=' text-subBlue text-[15px]  w-[35%] xs:w-[60%] font-light '>
+              Ongoing partnership with the university of Benin to integrate 70k
+              wallets
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className='bg-[#1358CD0D] my-20 py-10'>
+        <h1 className='text-center text-subBlue font-semibold text-[30px] mb-10'>
+          Why You Should Choose Us
+        </h1>
+        <div className='flex mx-auto w-[90%]'>
+          <div className='relative'>
+            <Image
+              src={whiteBg}
+              alt='whiteB background'
+              className='w-[368px] h-[367px]'
+            />
+            <h1 className='absolute top-[76px] left-[86px] bg-black w-[70px] h-[70px] rounded-full text-white font-bold text-[40px] flex items-center justify-center align-middle '>
+              1
+            </h1>
+            <h1 className='absolute top-[167px] left-[86px] text-[15px] w-[30%] font-semibold leading-4'>
+              Swift & Free Transactions
+            </h1>
+            <p className='absolute top-[218px] left-[86px] text-[12px] w-[50%]'>
+              All transactions on Ledga are totally free and fast
+            </p>
+          </div>
+          <div className='relative'>
+            <Image
+              src={whiteBg}
+              alt='whiteB background'
+              className='w-[368px] h-[367px]'
+            />
+            <h1 className='absolute top-[76px] left-[86px] bg-black w-[70px] h-[70px] rounded-full text-white font-bold text-[40px] flex items-center justify-center align-middle '>
+              2
+            </h1>
+            <h1 className='absolute top-[167px] left-[86px] text-[15px] w-[30%] font-semibold leading-4'>
+              Pocket Money Management
+            </h1>
+            <p className='absolute top-[218px] left-[86px] text-[12px] w-[50%]'>
+              ALedga helps your student manage pocket money and make payments
+            </p>
+          </div>
+          <div className='relative'>
+            <Image
+              src={whiteBg}
+              alt='whiteB background'
+              className='w-[368px] h-[367px]'
+            />
+            <h1 className='absolute top-[76px] left-[86px] bg-black w-[70px] h-[70px] rounded-full text-white font-bold text-[40px] flex items-center justify-center align-middle '>
+              3
+            </h1>
+            <h1 className='absolute top-[167px] left-[86px] text-[15px] w-[30%] font-semibold leading-4'>
+              Alternative Income Stream
+            </h1>
+            <p className='absolute top-[218px] left-[86px] text-[12px] w-[50%]'>
+              Earn every time your student funds their account
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* Big Blue Bg */}
+      <div className='relative'>
+        <Image
+          src={bigBlue}
+          alt='Big blue background'
+          className='w-[1461px] h-[854px]'
+        />
+        <div className='absolute top-[65px] left-[100px]'>
+          <h1 className='text-[30px]  font-semibold text-white'>
+            Who can use ledga?
+          </h1>
+          <p className='text-[18px] w-[45%] text-white'>
+            Ledga will be used by different organizations such as Schools,
+            Hotels, Hospitals and other organization that will need the services
+            of ledga
+          </p>
+        </div>
+        <div className='grid grid-cols-2 gap-10 absolute top-[350px] left-[100px]'>
+          <div className=' top-[65px] left-[100px]'>
+            <div className='flex gap-4 items-center align-middle '>
+              <div className='w-5 h-5 bg-white rounded-md shadow-sm'></div>
+              <h1 className='text-[25px]  font-semibold text-white'>Schools</h1>
+            </div>
+            <p className='text-[15px] w-[80%] text-white font-extralight'>
+              Schools Ledga helps schools streamline fee collection, manage
+              sub-accounts and member wallets, monitor financial flows, and
+              delegate roles, enhancing transparency and reducing administrative
+              burdens.
+            </p>
+          </div>
+          <div className=' '>
+            <div className='flex gap-4 items-center align-middle '>
+              <div className='w-5 h-5 bg-white rounded-md shadow-sm'></div>
+              <h1 className='text-[25px]  font-semibold text-white'>
+                Hospitals
+              </h1>
+            </div>
+            <p className='text-[15px] w-[80%] text-white font-extralight'>
+              Hospitals Hospitals can use Ledga to manage departmental finances,
+              patient payments, and expenses efficiently, ensuring transparent
+              transactions and simplified billing processes.
+            </p>
+          </div>
+          <div className=''>
+            <div className='flex gap-4 items-center align-middle '>
+              <div className='w-5 h-5 bg-white rounded-md shadow-sm'></div>
+              <h1 className='text-[25px]  font-semibold text-white'>Hotels</h1>
+            </div>
+            <p className='text-[15px] w-[80%] text-white font-extralight'>
+              Hotels Ledga enables hotels to handle booking payments, staff
+              payroll, and multiple accounts, improving financial oversight and
+              operational efficiency.
+            </p>
+          </div>
+          <div className=''>
+            <div className='flex gap-4 items-center align-middle '>
+              <div className='w-5 h-5 bg-white rounded-md shadow-sm'></div>
+              <h1 className='text-[25px]  font-semibold text-white'>
+                Organizations
+              </h1>
+            </div>
+            <p className='text-[15px] w-[80%] text-white font-extralight'>
+              Organizations Organizations can leverage Ledga to manage project
+              finances, peer transfers, and overall financial health with
+              transparent tracking and easy role delegation.
+            </p>
+          </div>
         </div>
       </div>
     </main>
